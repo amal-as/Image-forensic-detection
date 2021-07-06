f12c3679c4778ac069a6f5d3051e36b46a60208f
amal-as <69898599+amal-as@users.noreply.github.com>
2021-07-06 23:21:38 +0530

    Add files via upload

---
+++ forensic/DCTcheck.Designer.cs
@@ -0,0 +1,350 @@
+﻿namespace Forensic
+{
+    partial class DCTcheck
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.targetpic = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.orginalpic = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.suspdct = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.orginaldct = new System.Windows.Forms.PictureBox();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.targetpic)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.orginalpic)).BeginInit();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.suspdct)).BeginInit();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.orginaldct)).BeginInit();
+            this.panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.targetpic);
+            this.panel2.Location = new System.Drawing.Point(31, 439);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(266, 265);
+            this.panel2.TabIndex = 9;
+            // 
+            // targetpic
+            // 
+            this.targetpic.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.targetpic.Location = new System.Drawing.Point(0, 0);
+            this.targetpic.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.targetpic.Name = "targetpic";
+            this.targetpic.Size = new System.Drawing.Size(264, 263);
+            this.targetpic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.targetpic.TabIndex = 4;
+            this.targetpic.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label4.Location = new System.Drawing.Point(27, 420);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(117, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Suspected Image";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label3.Location = new System.Drawing.Point(32, 117);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Orginal Image";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.orginalpic);
+            this.panel1.Location = new System.Drawing.Point(29, 137);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(266, 265);
+            this.panel1.TabIndex = 7;
+            // 
+            // orginalpic
+            // 
+            this.orginalpic.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.orginalpic.Location = new System.Drawing.Point(0, 0);
+            this.orginalpic.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.orginalpic.Name = "orginalpic";
+            this.orginalpic.Size = new System.Drawing.Size(264, 263);
+            this.orginalpic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.orginalpic.TabIndex = 2;
+            this.orginalpic.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Light", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Silver;
+            this.label1.Location = new System.Drawing.Point(16, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(233, 106);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "DCT...";
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.suspdct);
+            this.panel3.Location = new System.Drawing.Point(336, 439);
+            this.panel3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(266, 265);
+            this.panel3.TabIndex = 13;
+            // 
+            // suspdct
+            // 
+            this.suspdct.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.suspdct.Location = new System.Drawing.Point(0, 0);
+            this.suspdct.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.suspdct.Name = "suspdct";
+            this.suspdct.Size = new System.Drawing.Size(264, 263);
+            this.suspdct.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.suspdct.TabIndex = 4;
+            this.suspdct.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label2.Location = new System.Drawing.Point(332, 420);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 17);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Suspected Image";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label5.Location = new System.Drawing.Point(337, 117);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(96, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Orginal Image";
+            // 
+            // panel4
+            // 
+            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel4.Controls.Add(this.orginaldct);
+            this.panel4.Location = new System.Drawing.Point(335, 137);
+            this.panel4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(266, 265);
+            this.panel4.TabIndex = 11;
+            // 
+            // orginaldct
+            // 
+            this.orginaldct.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.orginaldct.Location = new System.Drawing.Point(0, 0);
+            this.orginaldct.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.orginaldct.Name = "orginaldct";
+            this.orginaldct.Size = new System.Drawing.Size(264, 263);
+            this.orginaldct.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.orginaldct.TabIndex = 2;
+            this.orginaldct.TabStop = false;
+            this.orginaldct.Click += new System.EventHandler(this.orginaldct_Click);
+            // 
+            // panel5
+            // 
+            this.panel5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel5.Controls.Add(this.splitContainer1);
+            this.panel5.Location = new System.Drawing.Point(616, 613);
+            this.panel5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(331, 91);
+            this.panel5.TabIndex = 75;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(331, 91);
+            this.splitContainer1.SplitterDistance = 33;
+            this.splitContainer1.SplitterWidth = 5;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.button2);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.button3);
+            this.splitContainer2.Size = new System.Drawing.Size(293, 91);
+            this.splitContainer2.SplitterDistance = 147;
+            this.splitContainer2.SplitterWidth = 5;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DarkGray;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.DimGray;
+            this.button2.Image = global::Forensic.Properties.Resources.play;
+            this.button2.Location = new System.Drawing.Point(0, 0);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 91);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Next";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DarkGray;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.Color.DimGray;
+            this.button3.Image = global::Forensic.Properties.Resources.delete;
+            this.button3.Location = new System.Drawing.Point(0, 0);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(141, 91);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Close";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // DCTcheck
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(963, 740);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "DCTcheck";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DCTcheck";
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.targetpic)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.orginalpic)).EndInit();
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.suspdct)).EndInit();
+            this.panel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.orginaldct)).EndInit();
+            this.panel5.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox targetpic;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox orginalpic;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.PictureBox suspdct;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.PictureBox orginaldct;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ forensic/DCTcheck.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Forensic
+{
+    public partial class DCTcheck : Form
+    {
+        public DCTcheck()
+        {
+            InitializeComponent();
+            orginalpic.Image = (Image)Program.orginal;
+            targetpic.Image = (Image)Program.suspected;
+            orgDct();
+            orgDct2();
+            checkdct();
+        }
+
+        public void checkdct()
+        {
+            int h = 0, w = 0,m=0,d=0;
+            double per=0;
+            if (orginalpic.Image.Height > targetpic.Image.Height)
+            {
+                h = targetpic.Image.Height;
+            }
+            else if (orginalpic.Image.Height < targetpic.Image.Height)
+            {
+                h = orginalpic.Image.Height;
+            }
+            else
+            {
+                h = orginalpic.Image.Height;
+            }
+
+            if (orginalpic.Image.Width > targetpic.Image.Width)
+            {
+                w = targetpic.Image.Width;
+            }
+            else if (orginalpic.Image.Width < targetpic.Image.Width)
+            {
+                w = orginalpic.Image.Width;
+            }
+            else
+            {
+                w = orginalpic.Image.Width;
+            }
+
+            for (int i = 0; i < w; i++)
+            {
+                for (int j = 0; j < h; j++)
+                {
+
+                    Color originalColor = Program.orginal.GetPixel(i, j);
+                    Color susp = Program.suspected.GetPixel(i, j);
+                    if (originalColor.R == susp.R)
+                    {
+                        if (originalColor.G == susp.G)
+                        {
+                            if (originalColor.B == susp.B)
+                            {
+                                m++;
+                            }
+                            else
+                            {
+                                d++;
+                            }
+                        }
+                        else
+                        {
+                            d++;
+                        }
+                    }
+                    else
+                    {
+                        d++;
+                    }
+                }
+            }
+
+            int total = h * w;
+           
+            
+            if (d==0 || d==1 || d==2)
+            {
+                
+            }
+            else 
+            {
+               
+            }
+
+        }
+        public static string imgpath;
+        public static string s = "";
+        public Bitmap bmp;
+        FastDCT2D ImgDCT;
+        public int rec_width, rec_height;
+        public int scale = 25; // Scaling percentage
+        public int WindowSize = 256;  // Dimension of Image Selection Window
+        double[,] DCTCoefficients; //DCT Coefficient matrix
+
+        public void orgDct()
+        {
+            int x, y, width, height;
+            Bitmap InputImage = new Bitmap(Program.orginal);
+            Bitmap temp = (Bitmap)InputImage.Clone();
+            int iheight = orginalpic.Image.Height;
+            int iwidth = orginalpic.Image.Width;
+            if (iheight < iwidth)
+            {
+                WindowSize = iheight;
+            }
+            else
+            {
+                WindowSize = iwidth;
+            }
+
+            width = height = (int)(WindowSize * Convert.ToInt32(100) / 100);
+            bmp = new Bitmap(width, height, InputImage.PixelFormat);
+            rec_width = rec_height = (int)(512 * ((float)100 / 100));
+            x = (int)((float)0 * (100 / Convert.ToDouble(100)));
+            y = (int)((float)0 * (100 / Convert.ToDouble(100)));
+            width = height = (int)(rec_width * (100 / (float)scale));
+            if (width > WindowSize)
+            {
+                width = height = WindowSize;
+            }
+
+            Rectangle area = new Rectangle(x, y, width, height);
+
+
+            bmp = (Bitmap)InputImage.Clone(area, InputImage.PixelFormat);
+            
+            ImgDCT = new FastDCT2D(bmp, WindowSize);
+            ImgDCT.FastDCT();// Finding 2D DCT of Image
+
+            orginaldct.Image = (Image)ImgDCT.DCTMap;
+
+        }
+
+        public void orgDct2()
+        {
+            int x, y, width, height;
+            Bitmap InputImage = new Bitmap(Program.suspected);
+            Bitmap temp = (Bitmap)InputImage.Clone();
+            int iheight = targetpic.Image.Height;
+            int iwidth = targetpic.Image.Width;
+            if (iheight < iwidth)
+            {
+                WindowSize = iheight;
+            }
+            else
+            {
+                WindowSize = iwidth;
+            }
+
+            width = height = (int)(WindowSize * Convert.ToInt32(100) / 100);
+            bmp = new Bitmap(width, height, InputImage.PixelFormat);
+            rec_width = rec_height = (int)(512 * ((float)100 / 100));
+            x = (int)((float)0 * (100 / Convert.ToDouble(100)));
+            y = (int)((float)0 * (100 / Convert.ToDouble(100)));
+            width = height = (int)(rec_width * (100 / (float)scale));
+            if (width > WindowSize)
+            {
+                width = height = WindowSize;
+            }
+
+            Rectangle area = new Rectangle(x, y, width, height);
+
+
+            bmp = (Bitmap)InputImage.Clone(area, InputImage.PixelFormat);
+
+            ImgDCT = new FastDCT2D(bmp, WindowSize);
+            ImgDCT.FastDCT();// Finding 2D DCT of Image
+
+            suspdct.Image = (Image)ImgDCT.DCTMap;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Baseform OBJ = new Baseform();
+            ActiveForm.Hide();
+            OBJ.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Noise obj = new Noise((Bitmap)orginaldct.Image,(Bitmap)suspdct.Image);
+            ActiveForm.Hide();
+            obj.Show();
+        }
+
+        private void orginaldct_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
